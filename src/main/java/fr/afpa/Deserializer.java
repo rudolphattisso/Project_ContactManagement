1f398a7c34074f370c8247cd75efe14ae6eea148
RudolphAttisso <attissorudolph@gmail.com>
2024-07-30 14:49:38 +0200

    indatation de interface Deserialize

--- src/main/java/fr/afpa/Deserializer.java
+++ src/main/java/fr/afpa/Deserializer.java
@@ -2,13 +2,8 @@ package fr.afpa;
 
 import java.util.ArrayList;
 
-// 
-public interface Deserializer<T>{
-    
-   
-public abstract  String load(String filePath);
+public interface Deserializer<T> {
 
-public abstract ArrayList<T> loadList(String filePath);
-
-
+    public abstract String load(String filePath);
+    public abstract ArrayList<T> loadList(String filePath);
 }
