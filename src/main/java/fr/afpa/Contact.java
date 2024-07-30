365bfd287b6d676047e6c802f9afbf157bf7ee75
RudolphAttisso <attissorudolph@gmail.com>
2024-07-30 10:29:50 +0200

    modification du fichier Pom.XML pour rajouter les dépendences log4j2 et et création de la class contact

---
+++ src/main/java/fr/afpa/Contact.java
@@ -0,0 +1,18 @@
+package fr.afpa;
+
+public class Contact {
+
+    private String nom;
+    private String prénom;
+    private String genre;
+    private String dateDeNAissance;
+    private String pseudo;
+    private String adresse;
+    private String telPerso;
+    private String telPro;
+    private String mail;
+    private String codePostale;
+    private String lienGit ;
+
+    
+}
