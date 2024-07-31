dc78a3f8403b3d9152a30e62376bd1efa0909a3d
RudolphAttisso <attissorudolph@gmail.com>
2024-07-31 11:59:32 +0200

    lien entre la vue fusionnée et le controller; TO DO: implémenter mon les boutons

---
+++ src/main/java/fr/afpa/controllers/ContactManagementController.java
@@ -0,0 +1,92 @@
+package fr.afpa.controllers;
+
+import java.io.IOException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import fr.afpa.App;
+import fr.afpa.Contact;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class ContactManagementController {
+
+    private static Logger logger = LogManager.getLogger(App.class);
+    // tableView
+    @FXML
+    private TableView<Contact> tableViewContact;
+    @FXML
+    private TableColumn<Contact, String> colPrenom;
+
+    @FXML
+    private TableColumn<Contact, String> colNom;
+    @FXML
+    private TableColumn<Contact, String> colGenre;
+    @FXML
+    private TableColumn<Contact, String> colMail;
+    @FXML
+    private TableColumn<Contact, String> colTel;
+    // boutons
+    @FXML
+    private Button boutonJson;
+    @FXML
+    private Button boutonVCard;
+    @FXML
+    private Button boutonSupprimer;
+    @FXML
+    private Button boutonModifier;
+    @FXML
+    private Button boutonCreer;
+    // textFields
+    @FXML
+    private TextField nomfField;
+    @FXML
+    private TextField prenomField;
+    @FXML
+    private TextField genreField;
+    @FXML
+    private TextField dateDeNaissanceField;
+    @FXML
+    private TextField pseudoField;
+    @FXML
+    private TextField adresseField;
+    @FXML
+    private TextField numPersoField;
+    @FXML
+    private TextField numProField;
+    @FXML
+    private TextField mailField;
+    @FXML
+    private TextField lienGitField;
+
+    @FXML
+    public void jsonExport(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void vCardExport(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void creer(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void modifier(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void supprimer(ActionEvent event) {
+
+    }
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,10 @@
 module fr.afpa {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.apache.logging.log4j;
 
     opens fr.afpa to javafx.fxml;
+    opens fr.afpa.controllers to javafx.fxml;
     exports fr.afpa;
+    exports fr.afpa.controllers;
 }
